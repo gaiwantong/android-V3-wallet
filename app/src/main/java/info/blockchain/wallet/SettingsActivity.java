511cfd69b293ce04a1e17b01c8269a940baff98b
Riaan Vos <riaan.vos@blockchain.com>
2015-04-21 10:45:33 +0100

    Toolbar added to settings. Change PIN code functionality added.

--- app/src/main/java/info/blockchain/wallet/SettingsActivity.java
+++ app/src/main/java/info/blockchain/wallet/SettingsActivity.java
@@ -5,12 +5,20 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceClickListener;
 import android.preference.PreferenceActivity;
+import android.support.v7.widget.Toolbar;
 import android.text.InputType;
+import android.util.TypedValue;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.google.bitcoin.core.AddressFormatException;
@@ -41,6 +49,7 @@ public class SettingsActivity extends PreferenceActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setTitle(R.string.app_name);
         addPreferencesFromResource(R.xml.settings);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
@@ -388,6 +397,45 @@ public class SettingsActivity extends PreferenceActivity {
                 ).show();
 
     }
+
+	@Override
+	protected void onPostCreate(Bundle savedInstanceState){
+		super.onPostCreate(savedInstanceState);
+
+		Toolbar bar;
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
+			LinearLayout root = (LinearLayout) findViewById(android.R.id.list).getParent().getParent().getParent();
+			bar = (Toolbar) LayoutInflater.from(this).inflate(R.layout.toolbar_settings, root, false);
+			root.addView(bar, 0); // insert at top
+		} else {
+			ViewGroup root = (ViewGroup) findViewById(android.R.id.content);
+			ListView content = (ListView) root.getChildAt(0);
+
+			root.removeAllViews();
+
+			bar = (Toolbar) LayoutInflater.from(this).inflate(R.layout.toolbar_settings, root, false);
+
+			int height;
+			TypedValue tv = new TypedValue();
+			if (getTheme().resolveAttribute(R.attr.actionBarSize, tv, true)) {
+				height = TypedValue.complexToDimensionPixelSize(tv.data, getResources().getDisplayMetrics());
+			}else{
+				height = bar.getHeight();
+			}
+
+			content.setPadding(0, height, 0, 0);
+
+			root.addView(content);
+			root.addView(bar);
+		}
+
+		bar.setNavigationOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				finish();
+			}
+		});
+	}
 
 /*
     @Override
