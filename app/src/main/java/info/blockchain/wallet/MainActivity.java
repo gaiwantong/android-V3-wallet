0b53bbdc475469f130824be4f3e623d2c3e0187f
Riaan Vos <riaan.vos@blockchain.com>
2015-05-15 11:07:28 +0100

    Navigation Drawer set to standard width of 320dp.

--- app/src/main/java/info/blockchain/wallet/MainActivity.java
+++ app/src/main/java/info/blockchain/wallet/MainActivity.java
@@ -854,17 +854,6 @@ public class MainActivity extends ActionBarActivity implements CreateNdefMessage
 
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
 
-        // Fix right margin to 56dp (portrait)
-        View drawer = findViewById(R.id.scrimInsetsFrameLayout);
-        ViewGroup.LayoutParams layoutParams = drawer.getLayoutParams();
-        DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
-        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
-            layoutParams.width = displayMetrics.widthPixels - (56 * Math.round(displayMetrics.density));
-        }
-        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
-            layoutParams.width = displayMetrics.widthPixels + (20 * Math.round(displayMetrics.density)) - displayMetrics.widthPixels / 2;
-        }
-
         // Setup Drawer Icon
         mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, toolbar, R.string.drawer_open, R.string.drawer_close) {
 
