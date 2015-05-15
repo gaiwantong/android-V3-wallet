81f73077ac7c71d145e5f3cedf3f85b4406ee208
WilliamHill <william.hill@gmail.com>
2015-05-15 18:01:33 +0200

    Refactor try/catch staetement

--- app/src/main/java/info/blockchain/wallet/SendFragment.java
+++ app/src/main/java/info/blockchain/wallet/SendFragment.java
@@ -367,10 +367,7 @@ public class SendFragment extends Fragment {
                 try {
                     btc_amount = NumberFormat.getInstance(locale).parse(edAmount1.getText().toString()).doubleValue();
                 }
-                catch(NumberFormatException nfe) {
-                    btc_amount = 0.0;
-                }
-                catch(ParseException pe) {
+                catch(NumberFormatException | ParseException e) {
                     btc_amount = 0.0;
                 }
 
@@ -570,10 +567,7 @@ public class SendFragment extends Fragment {
 		try {
 			fiat_amount = NumberFormat.getInstance(locale).parse(cfiat).doubleValue();
 		}
-		catch(NumberFormatException nfe) {
-			fiat_amount = 0.0;
-		}
-		catch(ParseException pe) {
+		catch(NumberFormatException | ParseException e) {
 			fiat_amount = 0.0;
 		}
 		double btc_amount = fiat_amount / btc_fx;
@@ -998,11 +992,7 @@ public class SendFragment extends Fragment {
 				return false;
 			}
 		}
-		catch(NumberFormatException nfe) {
-			Toast.makeText(getActivity(), R.string.invalid_amount, Toast.LENGTH_SHORT).show();
-			return false;
-		}
-		catch(ParseException pe) {
+		catch(NumberFormatException | ParseException e) {
 			Toast.makeText(getActivity(), R.string.invalid_amount, Toast.LENGTH_SHORT).show();
 			return false;
 		}
