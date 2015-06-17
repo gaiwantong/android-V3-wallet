787c1bf2eb16bf77e19a589248f120790785927d
Riaan Vos <riaan.vos@blockchain.com>
2015-06-17 15:47:31 +0100

    UpgradeWalletActivity buttons added.

--- app/src/main/java/info/blockchain/wallet/UpgradeWalletActivity.java
+++ app/src/main/java/info/blockchain/wallet/UpgradeWalletActivity.java
@@ -3,6 +3,7 @@ package info.blockchain.wallet;
 import android.app.Activity;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.View;
 
 import info.blockchain.wallet.util.AppUtil;
 
@@ -20,5 +21,11 @@ public class UpgradeWalletActivity extends Activity {
     protected void onResume() {
         super.onResume();
         AppUtil.getInstance(UpgradeWalletActivity.this).updatePinEntryTime();
+    }
+
+    public void upgradeClicked(View view) {
+    }
+
+    public void askLaterClicked(View view) {
     }
 }
