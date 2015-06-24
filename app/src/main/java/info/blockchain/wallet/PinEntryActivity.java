9ca4c52c07e622bc8f66c854f19fa577510fcbb8
WilliamHill <william.hill@gmail.com>
2015-06-24 15:42:15 +0200

    PinEntryActivity: cleanup

--- app/src/main/java/info/blockchain/wallet/PinEntryActivity.java
+++ app/src/main/java/info/blockchain/wallet/PinEntryActivity.java
@@ -288,16 +288,8 @@ public class PinEntryActivity extends Activity {
                                     }
                                 }
 
-                                /*
-                                if(PrefsUtil.getInstance(PinEntryActivity.this).getValue(PrefsUtil.KEY_EMAIL_VERIFIED, false)){
-                                    AppUtil.getInstance(PinEntryActivity.this).restartApp("verified", true);
-                                }else{
-                                    Intent intent = new Intent(PinEntryActivity.this, ConfirmationCodeActivity.class);
-                                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
-                                    startActivity(intent);
-                                }
-                                */
-                            }
+                           }
+
                         });
 
                     }
