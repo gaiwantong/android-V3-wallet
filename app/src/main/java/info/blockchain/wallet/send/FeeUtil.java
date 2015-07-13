13c86f298399278c30baefc0faf551eaf489849b
WilliamHill <william.hill@gmail.com>
2015-07-13 09:10:11 +0200

    Estimate/calculate fess based on tx size

---
+++ app/src/main/java/info/blockchain/wallet/send/FeeUtil.java
@@ -0,0 +1,91 @@
+package info.blockchain.wallet.send;
+
+import java.math.BigInteger;
+import java.util.List;
+
+import com.google.bitcoin.core.Transaction;
+
+import org.spongycastle.util.encoders.Hex;
+
+public class FeeUtil {
+
+    private static final int ESTIMATED_INPUT_LEN = 148; // compressed key
+    private static final int ESTIMATED_OUTPUT_LEN = 34;
+
+    private static FeeUtil instance = null;
+
+    private FeeUtil()    { ; }
+
+    public static FeeUtil getInstance()  {
+
+        if(instance == null)    {
+            instance = new FeeUtil();
+        }
+
+        return instance;
+    }
+
+    public long getPriority(Transaction tx, List<MyTransactionOutPoint> outputs)   {
+
+        long priority = 0L;
+
+        for(MyTransactionOutPoint output : outputs)   {
+            priority += output.getValue().longValue() * output.getConfirmations();
+        }
+        //
+        // calculate priority
+        //
+        long estimatedSize = tx.bitcoinSerialize().length + (114 * tx.getInputs().size());
+        priority /= estimatedSize;
+
+        return priority;
+    }
+
+    public BigInteger calculatedFee(Transaction tx)   {
+
+        String hexString = new String(Hex.encode(tx.bitcoinSerialize()));
+        int size = hexString.length();
+
+        int thousands = size / 1000;
+        int remainder = size % 1000;
+
+        long fee = SendFactory.bFee.longValue() * thousands;
+        if(remainder > 0L)   {
+            fee += SendFactory.bFee.longValue();
+        }
+
+        return BigInteger.valueOf(fee);
+    }
+
+    public BigInteger estimatedFee(Transaction tx)   {
+
+        int size = estimatedSize(tx.getOutputs().size(), tx.getInputs().size());
+
+        return estimatedFee(size);
+    }
+
+    public BigInteger estimatedFee(int inputs, int outputs)   {
+
+        int size = estimatedSize(inputs, outputs);
+
+        return estimatedFee(size);
+    }
+
+    private BigInteger estimatedFee(int size)   {
+
+        int thousands = size / 1000;
+        int remainder = size % 1000;
+
+        long fee = SendFactory.bFee.longValue() * thousands;
+        if(remainder > 0L)   {
+            fee += SendFactory.bFee.longValue();
+        }
+
+        return BigInteger.valueOf(fee);
+    }
+
+    private int estimatedSize(int inputs, int outputs)   {
+        return (outputs * ESTIMATED_OUTPUT_LEN) + (inputs * ESTIMATED_INPUT_LEN) + inputs;
+    }
+
+}
