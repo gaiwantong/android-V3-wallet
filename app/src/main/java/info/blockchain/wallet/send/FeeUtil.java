b2151c758c095e329eee7f882fb2fb0046b7d062
WilliamHill <william.hill@gmail.com>
2015-07-27 15:47:00 +0100

    FeeUtil: min. fee for inclusion in blocks of 750KB, 1MB

--- app/src/main/java/info/blockchain/wallet/send/FeeUtil.java
+++ app/src/main/java/info/blockchain/wallet/send/FeeUtil.java
@@ -22,6 +22,8 @@ public class FeeUtil {
     private static BigInteger bHighestFee = null;
     private static BigInteger bPriorityFee = null;  // recommended priority fee
     private static double dPriorityMultiplier = 1.5;
+    private static BigInteger bMinFee750KB = null;
+    private static BigInteger bMinFee1MB = null;
     private static int totalBytes = -1;
 
     private static FeeUtil instance = null;
@@ -98,6 +100,8 @@ public class FeeUtil {
             averageFee(response);
             highestFee(response);
             totalBytes(response);
+            minFee750KB(response);
+            minFee1MB(response);
         }
         catch(Exception e) {
             ;
@@ -145,6 +149,54 @@ public class FeeUtil {
 
                 high_fee = nf.parse(value.trim()).doubleValue() / 1000;
                 bHighestFee = BigInteger.valueOf((long)(Math.round(high_fee * 1e8)));
+            }
+
+        }
+        catch(Exception e) {
+            ;
+        }
+
+    }
+
+    private void minFee750KB(String s) {
+
+        double min_fee = -1.0;
+
+        try {
+            Pattern pattern = Pattern.compile("Minimum fee/KB for 750KB block: ([.\\d]+)");
+            Matcher matcher = pattern.matcher(s);
+            if(matcher.find())  {
+                String value = matcher.group(1);
+
+                NumberFormat nf = NumberFormat.getInstance(Locale.US);
+                nf.setMaximumFractionDigits(8);
+
+                min_fee = nf.parse(value.trim()).doubleValue() / 1000;
+                bMinFee750KB = BigInteger.valueOf((long)(Math.round(min_fee * 1e8)));
+            }
+
+        }
+        catch(Exception e) {
+            ;
+        }
+
+    }
+
+    private void minFee1MB(String s) {
+
+        double min_fee = -1.0;
+
+        try {
+            Pattern pattern = Pattern.compile("Minimum fee/KB for 1MB block: ([.\\d]+)");
+            Matcher matcher = pattern.matcher(s);
+            if(matcher.find())  {
+                String value = matcher.group(1);
+
+                NumberFormat nf = NumberFormat.getInstance(Locale.US);
+                nf.setMaximumFractionDigits(8);
+
+                min_fee = nf.parse(value.trim()).doubleValue() / 1000;
+                bMinFee1MB = BigInteger.valueOf((long)(Math.round(min_fee * 1e8)));
             }
 
         }
