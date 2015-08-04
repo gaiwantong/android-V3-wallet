b0c361a42796a83aae59c51a2c29401dbf1133a1
Riaan Vos <riaan.vos@blockchain.com>
2015-08-04 15:45:12 +0100

    UITest - Create wallet.

--- app/src/androidTest/java/info/blockchain/ui/CreateAWalletTest.java
+++ app/src/androidTest/java/info/blockchain/ui/CreateAWalletTest.java
@@ -10,6 +10,8 @@ import junit.framework.TestCase;
 import java.util.ArrayList;
 
 import info.blockchain.wallet.LandingActivity;
+import info.blockchain.wallet.PinEntryActivity;
+import info.blockchain.wallet.PolicyActivity;
 import piuk.blockchain.android.R;
 
 public class CreateAWalletTest extends ActivityInstrumentationTestCase2<LandingActivity> {
@@ -93,10 +95,10 @@ public class CreateAWalletTest extends ActivityInstrumentationTestCase2<LandingA
         solo.clearEditText(walletPasswordConfirmView);
 
         ArrayList<String> strengthsSet = new ArrayList<>();
-        strengthsSet.add(0,solo.getString(R.string.strength_weak));
-        strengthsSet.add(1,solo.getString(R.string.strength_medium));
-        strengthsSet.add(2,solo.getString(R.string.strength_strong));
-        strengthsSet.add(3,solo.getString(R.string.strength_very_strong));
+        strengthsSet.add(0, solo.getString(R.string.strength_weak));
+        strengthsSet.add(1, solo.getString(R.string.strength_medium));
+        strengthsSet.add(2, solo.getString(R.string.strength_strong));
+        strengthsSet.add(3, solo.getString(R.string.strength_very_strong));
 
         for (int i = 0; i < strengthsSet.size(); i++) {
 
@@ -115,5 +117,79 @@ public class CreateAWalletTest extends ActivityInstrumentationTestCase2<LandingA
             //Test result
             TestCase.assertEquals("Password: '"+pw+"' strength not equal to: '"+strengthsSet.get(i)+"'", true, solo.waitForText(strengthsSet.get(i)));
         }
+    }
+
+    public void testBlockchainTermsOfServiceLink()  throws AssertionError{
+
+        //First hide keyboard
+        solo.pressSoftKeyboardNextButton();//skip email
+        solo.pressSoftKeyboardNextButton();//skip pw1
+        solo.pressSoftKeyboardNextButton();//skip pw2
+
+        solo.clickOnView(solo.getView(R.id.tos));
+
+        //Test result
+        TestCase.assertEquals(true, solo.waitForActivity(PolicyActivity.class));
+
+    }
+
+    public void testDefineNewPasswordModal()  throws AssertionError{
+
+        //Clear text fields
+        solo.clearEditText(emailAddressView);
+        solo.clearEditText(walletPasswordView);
+        solo.clearEditText(walletPasswordConfirmView);
+
+        //Enter details
+        String email = "qatest@qatest.com";
+        String pw = "aaaaaaaaa";
+        solo.enterText(emailAddressView, email);
+        solo.enterText(walletPasswordView, pw);
+        solo.enterText(walletPasswordConfirmView, pw);
+
+        //Complete
+        solo.clickOnView(solo.getView(R.id.command_next));
+
+        solo.waitForDialogToOpen();
+        solo.clickOnView(solo.getView(android.R.id.button1));//Yes
+        solo.waitForDialogToClose();
+
+        //Test result
+        TestCase.assertEquals(true, walletPasswordView.getText().toString().isEmpty());
+
+
+        solo.enterText(walletPasswordView, pw);
+        solo.enterText(walletPasswordConfirmView, pw);
+
+        //Complete
+        solo.clickOnView(solo.getView(R.id.command_next));
+
+        solo.waitForDialogToOpen();
+        solo.clickOnView(solo.getView(android.R.id.button2));//No
+        solo.waitForDialogToClose();
+
+        //Test result
+        TestCase.assertEquals(true, solo.waitForActivity(PinEntryActivity.class));
+    }
+
+    public void testCreateWallet()  throws AssertionError{
+
+        //Clear text fields
+        solo.clearEditText(emailAddressView);
+        solo.clearEditText(walletPasswordView);
+        solo.clearEditText(walletPasswordConfirmView);
+
+        //Enter details
+        String email = "qatest@qatest.com";
+        String pw = "!Very>Str0ng?Pass80*%";
+        solo.enterText(emailAddressView, email);
+        solo.enterText(walletPasswordView, pw);
+        solo.enterText(walletPasswordConfirmView, pw);
+
+        //Complete
+        solo.clickOnView(solo.getView(R.id.command_next));
+
+        //Test result
+        TestCase.assertEquals(true, solo.waitForActivity(PinEntryActivity.class));
     }
 }
