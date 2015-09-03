e7fdf11dd8f7281187ac089af1246cf766bb553c
Riaan Vos <riaan.vos@blockchain.com>
2015-09-03 11:29:26 +0100

    Automated UI tests: Balance screen bug fix.

--- app/src/androidTest/java/info/blockchain/ui/BalanceScreenTest.java
+++ app/src/androidTest/java/info/blockchain/ui/BalanceScreenTest.java
@@ -3,6 +3,7 @@ package info.blockchain.ui;
 import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.test.ActivityInstrumentationTestCase2;
+import android.util.TypedValue;
 import android.view.View;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -104,11 +105,16 @@ public class BalanceScreenTest extends ActivityInstrumentationTestCase2<MainActi
         }
     }
 
+    private float toPx(int dp){
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, getActivity().getResources().getDisplayMetrics());
+    }
+
     public void testC_BasicUI() throws AssertionError{
 
         Spinner mSpinner = solo.getView(Spinner.class, 0);
         int itemCount = mSpinner.getAdapter().getCount();
         View spinnerView = solo.getView(Spinner.class, 0);
+        txList = (RecyclerView) solo.getView(R.id.txList2);
 
         for(int i = itemCount-1; i >= 0; i--){
 
@@ -117,17 +123,23 @@ public class BalanceScreenTest extends ActivityInstrumentationTestCase2<MainActi
             solo.clickOnView(solo.getView(TextView.class, i));
             try{solo.sleep(1000);}catch (Exception e){}
 
-            txList = (RecyclerView) solo.getView(R.id.txList2);
             if(txList.getAdapter().getItemCount()==0)continue;
 
+            float actionBarHeight = toPx(56);
+            float headerHeight = toPx(72);
+            float rowHeight = toPx(72);
+            float yd = actionBarHeight + headerHeight + (float) (rowHeight/2.0);
+
+            solo.clickOnScreen(50,  yd);
+            try{solo.sleep(200);}catch (Exception e){}
+
             //Test if expanded
-            solo.clickOnView(txList.getChildAt(0));
             TestCase.assertTrue(solo.waitForText(solo.getString(R.string.from)) && solo.waitForText(solo.getString(R.string.transaction_fee)));
 
             //Toggle status/confirmation amount
-            solo.clickOnView(solo.getView(R.id.transaction_status));
-            solo.clickOnView(solo.getView(R.id.transaction_status));
-            solo.clickOnText(solo.getString(R.string.from));
+            solo.clickOnScreen(info.blockchain.wallet.util.DeviceUtil.getInstance(getActivity()).getWidth() - toPx(50), yd+rowHeight);
+            solo.clickOnScreen(info.blockchain.wallet.util.DeviceUtil.getInstance(getActivity()).getWidth() - toPx(50), yd+rowHeight);
+            solo.clickOnScreen(50,  yd);
 
             //Test scrolling
             txList.fling(0, 20000);
