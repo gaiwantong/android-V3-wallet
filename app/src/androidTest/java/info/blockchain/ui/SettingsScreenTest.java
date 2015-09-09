3400267c0b875ba34cc05a189a5efd090b44ec28
Riaan Vos <riaan.vos@blockchain.com>
2015-09-09 11:21:53 +0100

    Automated UI tests: SettingsScreenTest select currency and test change in balance screen.

--- app/src/androidTest/java/info/blockchain/ui/SettingsScreenTest.java
+++ app/src/androidTest/java/info/blockchain/ui/SettingsScreenTest.java
@@ -8,6 +8,7 @@ import com.robotium.solo.Solo;
 
 import info.blockchain.ui.util.UiUtil;
 import info.blockchain.wallet.MainActivity;
+import info.blockchain.wallet.util.ExchangeRateFactory;
 import info.blockchain.wallet.util.MonetaryUtil;
 import info.blockchain.wallet.util.PrefsUtil;
 import piuk.blockchain.android.R;
@@ -82,6 +83,44 @@ public class SettingsScreenTest extends ActivityInstrumentationTestCase2<MainAct
             }
 
             assertTrue(solo.searchText(units[sel].toString()));
+
+            UiUtil.getInstance(getActivity()).openNavigationDrawer(solo);
+            try{solo.sleep(500);}catch (Exception e){}
+            solo.clickOnText(getActivity().getString(R.string.action_settings));
+            try{solo.sleep(500);}catch (Exception e){}
+        }
+    }
+
+    public void testC_SelectCurrency() throws AssertionError{
+
+        final String[] currencies = ExchangeRateFactory.getInstance(getActivity()).getCurrencies();
+
+        for(String currency : currencies) {
+
+            if(solo.searchText(getActivity().getString(R.string.options_currency)))
+                solo.clickOnText(getActivity().getString(R.string.options_currency), 1, true);
+
+            solo.waitForText(getActivity().getString(R.string.select_currency), 1, 500, true);
+
+            boolean down = true;
+            while(!solo.searchText(currency.toString())){
+                if(down) {
+                    solo.scrollDown();
+                    down=!down;
+                }else
+
+                    solo.scrollUp();
+            }
+
+            solo.clickOnText(currency.toString(), 1, true);
+            try{solo.sleep(500);}catch (Exception e){}
+            solo.goBack();
+            while(!solo.searchText(currency)){
+                TextView balance = (TextView)solo.getView(R.id.balance1);
+                solo.clickOnView(balance);
+            }
+
+            assertTrue(solo.searchText(currency));
 
             UiUtil.getInstance(getActivity()).openNavigationDrawer(solo);
             try{solo.sleep(500);}catch (Exception e){}
