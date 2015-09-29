9721c46469d110ce3c63b8136692eab47159c2d3
Matt Tuzzolo <matt.tuzzolo@gmail.com>
2015-09-29 19:30:56 +0100

    tabs -> spaces

--- app/src/main/java/info/blockchain/wallet/service/WebSocketService.java
+++ app/src/main/java/info/blockchain/wallet/service/WebSocketService.java
@@ -1,8 +1,5 @@
 package info.blockchain.wallet.service;
 
-import java.util.Timer;
-import java.util.TimerTask;
-
 import android.app.NotificationManager;
 import android.content.Context;
 import android.content.Intent;
@@ -11,55 +8,57 @@ import android.net.ConnectivityManager;
 import android.os.Binder;
 import android.os.Handler;
 import android.os.IBinder;
-//import android.util.Log;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 import info.blockchain.wallet.payload.PayloadFactory;
 
+//import android.util.Log;
+
 public class WebSocketService extends android.app.Service	{
 
-	private static final long checkIfNotConnectedDelay = 15000L;
+    private static final long checkIfNotConnectedDelay = 15000L;
 
-	private WebSocketHandler webSocketHandler = null;
+    private WebSocketHandler webSocketHandler = null;
 
-	Timer timer = new Timer();
+    Timer timer = new Timer();
 
-	private final Handler handler = new Handler();
+    private final Handler handler = new Handler();
 
-	private NotificationManager nm;
-	private static final int NOTIFICATION_ID_CONNECTED = 0;
-	private static final int NOTIFICATION_ID_COINS_RECEIVED = 1;
-	private static final int NOTIFICATION_ID_COINS_SENT = 3;
-	public static boolean isRunning = false;
+    private NotificationManager nm;
+    private static final int NOTIFICATION_ID_CONNECTED = 0;
+    public static boolean isRunning = false;
 
-	public class LocalBinder extends Binder
-	{
-		public WebSocketService getService()
-		{
-			return WebSocketService.this;
-		}
-	}
+    public class LocalBinder extends Binder
+    {
+        public WebSocketService getService()
+        {
+            return WebSocketService.this;
+        }
+    }
 
-	private final IBinder mBinder = new LocalBinder();
+    private final IBinder mBinder = new LocalBinder();
 
-	@Override
-	public IBinder onBind(final Intent intent)
-	{
-		return mBinder;
-	}
+    @Override
+    public IBinder onBind(final Intent intent)
+    {
+        return mBinder;
+    }
 
-	@Override
-	public void onCreate()
-	{
-		super.onCreate();
+    @Override
+    public void onCreate()
+    {
+        super.onCreate();
 
-		isRunning = true;
-		
-		nm = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        isRunning = true;
 
-		final IntentFilter intentFilter = new IntentFilter();
-		intentFilter.addAction(ConnectivityManager.CONNECTIVITY_ACTION);
+        nm = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
-		int nbAccounts = 0;
+        final IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(ConnectivityManager.CONNECTIVITY_ACTION);
+
+        int nbAccounts = 0;
         if(PayloadFactory.getInstance().get().isUpgraded())    {
             try {
                 nbAccounts = PayloadFactory.getInstance().get().getHdWallet().getAccounts().size();
@@ -69,79 +68,78 @@ public class WebSocketService extends android.app.Service	{
             }
         }
 
-		final String[] xpubs = new String[nbAccounts];
-		for(int i = 0; i < nbAccounts; i++) {
-			String s = PayloadFactory.getInstance().get().getHdWallet().getAccounts().get(i).getXpub();
-			if(s != null && s.length() > 0) {
-				xpubs[i] = s;
-			}
-		}
+        final String[] xpubs = new String[nbAccounts];
+        for(int i = 0; i < nbAccounts; i++) {
+            String s = PayloadFactory.getInstance().get().getHdWallet().getAccounts().get(i).getXpub();
+            if(s != null && s.length() > 0) {
+                xpubs[i] = s;
+            }
+        }
 
-		int nbLegacy = PayloadFactory.getInstance().get().getLegacyAddresses().size();
-		final String[] addrs = new String[nbLegacy];
-		for(int i = 0; i < nbLegacy; i++) {
-			String s = PayloadFactory.getInstance().get().getLegacyAddresses().get(i).getAddress();
-			if(s != null && s.length() > 0) {
-				addrs[i] = PayloadFactory.getInstance().get().getLegacyAddresses().get(i).getAddress();
-			}
-		}
+        int nbLegacy = PayloadFactory.getInstance().get().getLegacyAddresses().size();
+        final String[] addrs = new String[nbLegacy];
+        for(int i = 0; i < nbLegacy; i++) {
+            String s = PayloadFactory.getInstance().get().getLegacyAddresses().get(i).getAddress();
+            if(s != null && s.length() > 0) {
+                addrs[i] = PayloadFactory.getInstance().get().getLegacyAddresses().get(i).getAddress();
+            }
+        }
 
-		webSocketHandler = new WebSocketHandler(WebSocketService.this, PayloadFactory.getInstance().get().getGuid(), xpubs, addrs);
-//					connectToWebsocketIfNotConnected();
+        webSocketHandler = new WebSocketHandler(WebSocketService.this, PayloadFactory.getInstance().get().getGuid(), xpubs, addrs);
 
-		timer.scheduleAtFixedRate(new TimerTask() {
-			@Override
-			public void run() {
-				handler.post(new Runnable() {
-					@Override
-					public void run() {
-						connectToWebsocketIfNotConnected();
-					}
-				});
-			}
-		}, 5000, checkIfNotConnectedDelay);
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        connectToWebsocketIfNotConnected();
+                    }
+                });
+            }
+        }, 5000, checkIfNotConnectedDelay);
 
-	}
+    }
 
-	public void connectToWebsocketIfNotConnected()
-	{
-		try {
-			if(webSocketHandler != null && !webSocketHandler.isConnected()) {
-				webSocketHandler.start();
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+    public void connectToWebsocketIfNotConnected()
+    {
+        try {
+            if(webSocketHandler != null && !webSocketHandler.isConnected()) {
+                webSocketHandler.start();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-	public void stop() {
-		try {
-			if(webSocketHandler != null)	{
-				webSocketHandler.stop();
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+    public void stop() {
+        try {
+            if(webSocketHandler != null)	{
+                webSocketHandler.stop();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-	@Override
-	public void onDestroy()
-	{
-		isRunning = false;
-		
-		stop();
+    @Override
+    public void onDestroy()
+    {
+        isRunning = false;
 
-		handler.removeCallbacksAndMessages(null);
+        stop();
 
-		handler.postDelayed(new Runnable()
-		{
-			public void run()
-			{
-				nm.cancel(NOTIFICATION_ID_CONNECTED);
-			}
-		}, 2000);
+        handler.removeCallbacksAndMessages(null);
 
-		super.onDestroy();
-	}
+        handler.postDelayed(new Runnable()
+        {
+            public void run()
+            {
+                nm.cancel(NOTIFICATION_ID_CONNECTED);
+            }
+        }, 2000);
+
+        super.onDestroy();
+    }
 
 }
