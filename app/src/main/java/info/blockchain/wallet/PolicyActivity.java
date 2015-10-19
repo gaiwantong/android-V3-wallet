2119f289b4e03ac9a7cef79cafda06f22d939da7
Riaan Vos <riaan.vos@blockchain.com>
2015-10-19 16:01:05 +0100

    Fixed Webview doesn't load successfuly every time.

--- app/src/main/java/info/blockchain/wallet/PolicyActivity.java
+++ app/src/main/java/info/blockchain/wallet/PolicyActivity.java
@@ -11,16 +11,12 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.ProgressBar;
 
 import info.blockchain.wallet.util.AppUtil;
-import info.blockchain.wallet.util.DeviceUtil;
-import piuk.blockchain.android.R;
 
 public class PolicyActivity extends Activity	{
 
     private WebView webview = null;
-    private ProgressBar progressBar = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,18 +24,16 @@ public class PolicyActivity extends Activity	{
 
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        this.setContentView(R.layout.activity_policy);
 
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
-        webview = (WebView)findViewById(R.id.webView);
+        webview = new WebView(this);
         webview.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
             return event.getAction() == MotionEvent.ACTION_UP;
             }
         });
-        progressBar = (ProgressBar)findViewById(R.id.progressSpinner);
 
         String uri = this.getIntent().getStringExtra("uri");
         if(uri.contains(".pdf")) {
@@ -50,26 +44,17 @@ public class PolicyActivity extends Activity	{
         }else {
 
             webview.getSettings().setTextZoom(60);
-            webview.setWebViewClient(new Browser());
             webview.loadUrl(uri);
+            webview.setWebViewClient(new WebViewClient() {
+                @Override
+                public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                    view.loadUrl(url);
+                    return true;
+                }
 
-            progressBar.setVisibility(View.VISIBLE);
-        }
-    }
+            });
 
-    private class Browser extends WebViewClient {
-        @Override
-        public boolean shouldOverrideUrlLoading(WebView view, String url) {
-            view.loadUrl(url);
-            return true;
-        }
-
-        @Override
-        public void onPageFinished(WebView view, String url) {
-            super.onPageFinished(view, url);
-            progressBar.setVisibility(View.GONE);
-            webview.setMinimumHeight(DeviceUtil.getInstance(PolicyActivity.this).getHeight());
-            webview.invalidate();
+            this.setContentView(webview);
         }
     }
 
