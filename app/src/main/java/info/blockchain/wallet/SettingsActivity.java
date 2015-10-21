6e6ae3e1121090e29690f74450f74eb656bbd48e
WilliamHill <william.hill@gmail.com>
2015-10-21 10:38:06 +0100

    Show toast when guid copied to clipboard

--- app/src/main/java/info/blockchain/wallet/SettingsActivity.java
+++ app/src/main/java/info/blockchain/wallet/SettingsActivity.java
@@ -67,6 +67,7 @@ public class SettingsActivity extends PreferenceActivity {
                                 android.content.ClipData clip = null;
                                 clip = android.content.ClipData.newPlainText("guid", guid);
                                 clipboard.setPrimaryClip(clip);
+                                ToastCustom.makeText(SettingsActivity.this, getString(R.string.copied_to_clipboard), ToastCustom.LENGTH_SHORT, ToastCustom.TYPE_ERROR);
                             }
 
                         }).setNegativeButton(R.string.no, new DialogInterface.OnClickListener() {
