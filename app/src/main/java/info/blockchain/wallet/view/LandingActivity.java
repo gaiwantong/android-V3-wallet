d7927b627b3af4f9685941b63124a3e809bd156f
Riaan Vos <riaan.vos@blockchain.com>
2016-07-19 11:39:26 +0100

    Databinding - LandingActivity

--- app/src/main/java/info/blockchain/wallet/view/LandingActivity.java
+++ app/src/main/java/info/blockchain/wallet/view/LandingActivity.java
@@ -5,35 +5,31 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnTouchListener;
-import android.widget.Button;
-import android.widget.TextView;
 
 import info.blockchain.wallet.access.AccessState;
 import info.blockchain.wallet.connectivity.ConnectivityStatus;
 import info.blockchain.wallet.util.AppUtil;
 
 import piuk.blockchain.android.R;
-
-//import android.util.Log;
+import piuk.blockchain.android.databinding.ActivityLandingBinding;
 
 public class LandingActivity extends Activity {
 
-    private Button btCreate = null;
-    private TextView btLogin = null;
+    private ActivityLandingBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_landing);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_landing);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         setTitle(R.string.app_name);
 
-        btCreate = (Button) findViewById(R.id.create);
-        btCreate.setOnTouchListener(new OnTouchListener() {
+        binding.create.setOnTouchListener(new OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
 
@@ -46,8 +42,7 @@ public class LandingActivity extends Activity {
             }
         });
 
-        btLogin = (TextView) findViewById(R.id.login);
-        btLogin.setOnTouchListener(new OnTouchListener() {
+        binding.login.setOnTouchListener(new OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
 
