9309e3028e411ccacef812a2878d9993f6e18325
Riaan Vos <riaan.vos@blockchain.com>
2016-07-19 13:35:51 +0100

    Databinding - UpgradeWalletActivity

--- app/src/main/java/info/blockchain/wallet/view/UpgradeWalletActivity.java
+++ app/src/main/java/info/blockchain/wallet/view/UpgradeWalletActivity.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.databinding.DataBindingUtil;
 import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -22,59 +23,48 @@ import android.view.animation.AnimationUtils;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.ProgressBar;
-import android.widget.TextSwitcher;
 import android.widget.TextView;
 import android.widget.ViewSwitcher;
 
 import info.blockchain.wallet.access.AccessState;
 import info.blockchain.wallet.connectivity.ConnectivityStatus;
 import info.blockchain.wallet.payload.PayloadManager;
-import info.blockchain.wallet.view.helpers.ToastCustom;
 import info.blockchain.wallet.util.AppUtil;
 import info.blockchain.wallet.util.CharSequenceX;
 import info.blockchain.wallet.util.DoubleEncryptionFactory;
 import info.blockchain.wallet.util.OSUtil;
 import info.blockchain.wallet.util.PasswordUtil;
 import info.blockchain.wallet.util.PrefsUtil;
+import info.blockchain.wallet.view.helpers.ToastCustom;
 
 import piuk.blockchain.android.R;
+import piuk.blockchain.android.databinding.ActivityUpgradeWalletBinding;
+import piuk.blockchain.android.databinding.ActivityUpgradeWalletConfirmBinding;
 
 public class UpgradeWalletActivity extends Activity {
 
     private AlertDialog alertDialog = null;
-    private ViewPager mViewPager = null;
     private CustomPagerAdapter mCustomPagerAdapter = null;
 
-    private TextSwitcher pageHeader = null;
-
-    private TextView pageBox0 = null;
-    private TextView pageBox1 = null;
-    private TextView pageBox2 = null;
-
-    private TextView heading = null;
-    private TextView info = null;
-    private ProgressBar progressBar = null;
-    private TextView confirmCancel = null;
-    private TextView confirmUpgrade = null;
     private PrefsUtil prefs;
     private AppUtil appUtil;
     private PayloadManager payloadManager;
+
+    private ActivityUpgradeWalletBinding binding;
+    private ActivityUpgradeWalletConfirmBinding dialogBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_upgrade_wallet);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_upgrade_wallet);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
         payloadManager = PayloadManager.getInstance();
         prefs = new PrefsUtil(this);
         appUtil = new AppUtil(this);
 
-        mViewPager = (ViewPager) findViewById(R.id.pager);
-        pageHeader = (TextSwitcher) findViewById(R.id.upgrade_page_header);
-        pageHeader.setFactory(new ViewSwitcher.ViewFactory() {
+        binding.upgradePageHeader.setFactory(new ViewSwitcher.ViewFactory() {
 
             public View makeView() {
                 TextView myText = new TextView(UpgradeWalletActivity.this);
@@ -84,13 +74,13 @@ public class UpgradeWalletActivity extends Activity {
                 return myText;
             }
         });
-        pageHeader.setInAnimation(AnimationUtils.loadAnimation(this, R.anim.abc_fade_in));
-        pageHeader.setOutAnimation(AnimationUtils.loadAnimation(this, R.anim.abc_fade_out));
-        pageHeader.setText(getResources().getString(R.string.upgrade_page_2));
+        binding.upgradePageHeader.setInAnimation(AnimationUtils.loadAnimation(this, R.anim.abc_fade_in));
+        binding.upgradePageHeader.setOutAnimation(AnimationUtils.loadAnimation(this, R.anim.abc_fade_out));
+        binding.upgradePageHeader.setText(getResources().getString(R.string.upgrade_page_2));
 
         mCustomPagerAdapter = new CustomPagerAdapter(this);
-        mViewPager.setAdapter(mCustomPagerAdapter);
-        mViewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+        binding.pager.setAdapter(mCustomPagerAdapter);
+        binding.pager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
             @Override
             public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
 
@@ -167,28 +157,24 @@ public class UpgradeWalletActivity extends Activity {
                     }).show();
 
         }
-
-        pageBox0 = (TextView) findViewById(R.id.pageBox0);
-        pageBox1 = (TextView) findViewById(R.id.pageBox1);
-        pageBox2 = (TextView) findViewById(R.id.pageBox2);
     }
 
     public void upgradeClicked(View view) {
         AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
-        LayoutInflater inflater = this.getLayoutInflater();
-        View dialogView = inflater.inflate(R.layout.activity_upgrade_wallet_confirm, null);
-        dialogBuilder.setView(dialogView);
+        dialogBinding = DataBindingUtil.inflate(LayoutInflater.from(this),
+                R.layout.activity_upgrade_wallet_confirm, null, false);
+        dialogBuilder.setView(dialogBinding.getRoot());
 
         alertDialog = dialogBuilder.create();
         alertDialog.setCanceledOnTouchOutside(false);
         alertDialog.setCancelable(false);
 
-        heading = (TextView) dialogView.findViewById(R.id.heading_tv);
-        info = (TextView) dialogView.findViewById(R.id.upgrade_info_tv);
-        progressBar = (ProgressBar) dialogView.findViewById(R.id.progressBar3);
+//        heading = (TextView) dialogView.findViewById(R.id.heading_tv);
+//        info = (TextView) dialogView.findViewById(R.id.upgrade_info_tv);
+//        progressBar = (ProgressBar) dialogView.findViewById(R.id.progressBar3);
 
-        confirmCancel = (TextView) dialogView.findViewById(R.id.confirm_cancel);
-        confirmCancel.setOnClickListener(new View.OnClickListener() {
+//        confirmCancel = (TextView) dialogView.findViewById(R.id.confirm_cancel);
+        dialogBinding.confirmCancel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
@@ -199,8 +185,8 @@ public class UpgradeWalletActivity extends Activity {
             }
         });
 
-        confirmUpgrade = (TextView) dialogView.findViewById(R.id.confirm_upgrade);
-        confirmUpgrade.setOnClickListener(new View.OnClickListener() {
+//        confirmUpgrade = (TextView) dialogView.findViewById(R.id.confirm_upgrade);
+        dialogBinding.confirmUpgrade.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
@@ -304,11 +290,11 @@ public class UpgradeWalletActivity extends Activity {
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                heading.setText(getString(R.string.upgrading));
-                info.setText(getString(R.string.upgrading_started_info));
-                progressBar.setVisibility(View.VISIBLE);
-                confirmCancel.setVisibility(View.INVISIBLE);
-                confirmUpgrade.setVisibility(View.INVISIBLE);
+                dialogBinding.headingTv.setText(getString(R.string.upgrading));
+                dialogBinding.upgradeInfoTv.setText(getString(R.string.upgrading_started_info));
+                dialogBinding.progressBar3.setVisibility(View.VISIBLE);
+                dialogBinding.confirmCancel.setVisibility(View.INVISIBLE);
+                dialogBinding.confirmUpgrade.setVisibility(View.INVISIBLE);
             }
         });
     }
@@ -322,12 +308,12 @@ public class UpgradeWalletActivity extends Activity {
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                heading.setText(getString(R.string.upgrade_success_heading));
-                info.setText(getString(R.string.upgrade_success_info));
-                progressBar.setVisibility(View.GONE);
-                confirmUpgrade.setVisibility(View.VISIBLE);
-                confirmUpgrade.setText(getString(R.string.CLOSE));
-                confirmUpgrade.setOnClickListener(new View.OnClickListener() {
+                dialogBinding.headingTv.setText(getString(R.string.upgrade_success_heading));
+                dialogBinding.upgradeInfoTv.setText(getString(R.string.upgrade_success_info));
+                dialogBinding.progressBar3.setVisibility(View.GONE);
+                dialogBinding.confirmUpgrade.setVisibility(View.VISIBLE);
+                dialogBinding.confirmUpgrade.setText(getString(R.string.CLOSE));
+                dialogBinding.confirmUpgrade.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
                         if (alertDialog != null && alertDialog.isShowing()) alertDialog.cancel();
@@ -351,12 +337,12 @@ public class UpgradeWalletActivity extends Activity {
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                heading.setText(getString(R.string.upgrade_fail_heading));
-                info.setText(getString(R.string.upgrade_fail_info));
-                progressBar.setVisibility(View.GONE);
-                confirmUpgrade.setVisibility(View.VISIBLE);
-                confirmUpgrade.setText(getString(R.string.CLOSE));
-                confirmUpgrade.setOnClickListener(new View.OnClickListener() {
+                dialogBinding.headingTv.setText(getString(R.string.upgrade_fail_heading));
+                dialogBinding.upgradeInfoTv.setText(getString(R.string.upgrade_fail_info));
+                dialogBinding.progressBar3.setVisibility(View.GONE);
+                dialogBinding.confirmUpgrade.setVisibility(View.VISIBLE);
+                dialogBinding.confirmUpgrade.setText(getString(R.string.CLOSE));
+                dialogBinding.confirmUpgrade.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
                         if (alertDialog != null && alertDialog.isShowing()) alertDialog.cancel();
@@ -377,22 +363,22 @@ public class UpgradeWalletActivity extends Activity {
 
     private void setSelectedPage(int position) {
 
-        pageBox0.setBackgroundDrawable(getResources().getDrawable(R.drawable.rounded_view_dark_blue));
-        pageBox1.setBackgroundDrawable(getResources().getDrawable(R.drawable.rounded_view_dark_blue));
-        pageBox2.setBackgroundDrawable(getResources().getDrawable(R.drawable.rounded_view_dark_blue));
+        binding.pageBox0.setBackgroundDrawable(getResources().getDrawable(R.drawable.rounded_view_dark_blue));
+        binding.pageBox1.setBackgroundDrawable(getResources().getDrawable(R.drawable.rounded_view_dark_blue));
+        binding.pageBox2.setBackgroundDrawable(getResources().getDrawable(R.drawable.rounded_view_dark_blue));
 
         switch (position) {
             case 0:
-                pageHeader.setText(getResources().getString(R.string.upgrade_page_2));
-                pageBox0.setBackgroundDrawable(getResources().getDrawable(R.drawable.rounded_view_upgrade_wallet_blue));
+                binding.upgradePageHeader.setText(getResources().getString(R.string.upgrade_page_2));
+                binding.pageBox0.setBackgroundDrawable(getResources().getDrawable(R.drawable.rounded_view_upgrade_wallet_blue));
                 break;
             case 1:
-                pageHeader.setText(getResources().getString(R.string.upgrade_page_3));
-                pageBox1.setBackgroundDrawable(getResources().getDrawable(R.drawable.rounded_view_upgrade_wallet_blue));
+                binding.upgradePageHeader.setText(getResources().getString(R.string.upgrade_page_3));
+                binding.pageBox1.setBackgroundDrawable(getResources().getDrawable(R.drawable.rounded_view_upgrade_wallet_blue));
                 break;
             case 2:
-                pageHeader.setText(getResources().getString(R.string.upgrade_page_1));
-                pageBox2.setBackgroundDrawable(getResources().getDrawable(R.drawable.rounded_view_upgrade_wallet_blue));
+                binding.upgradePageHeader.setText(getResources().getString(R.string.upgrade_page_1));
+                binding.pageBox2.setBackgroundDrawable(getResources().getDrawable(R.drawable.rounded_view_upgrade_wallet_blue));
                 break;
         }
     }
