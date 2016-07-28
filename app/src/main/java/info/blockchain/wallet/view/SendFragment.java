50cbe3de6804616424251eb7feba59abaca5c20a
Adam Bennett <adam@blockchain.com>
2016-07-28 10:44:53 +0100

    Fix for fiat value not parsing to double when using comma as decimal point

--- app/src/main/java/info/blockchain/wallet/view/SendFragment.java
+++ app/src/main/java/info/blockchain/wallet/view/SendFragment.java
@@ -1,8 +1,5 @@
 package info.blockchain.wallet.view;
 
-import com.google.common.collect.HashBiMap;
-import com.google.zxing.client.android.CaptureActivity;
-
 import android.Manifest;
 import android.app.Activity;
 import android.app.AlertDialog;
@@ -46,6 +43,26 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.common.collect.HashBiMap;
+import com.google.zxing.client.android.CaptureActivity;
+
+import org.apache.commons.lang3.StringUtils;
+import org.bitcoinj.core.Base58;
+import org.bitcoinj.core.ECKey;
+import org.bitcoinj.crypto.BIP38PrivateKey;
+import org.bitcoinj.params.MainNetParams;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+
 import info.blockchain.wallet.app_rate.AppRate;
 import info.blockchain.wallet.callbacks.CustomKeypadCallback;
 import info.blockchain.wallet.callbacks.OpCallback;
@@ -76,24 +93,6 @@ import info.blockchain.wallet.util.WebUtil;
 import info.blockchain.wallet.view.helpers.CustomKeypad;
 import info.blockchain.wallet.view.helpers.ToastCustom;
 import info.blockchain.wallet.viewModel.SendViewModel;
-
-import org.apache.commons.lang3.StringUtils;
-import org.bitcoinj.core.Base58;
-import org.bitcoinj.core.ECKey;
-import org.bitcoinj.crypto.BIP38PrivateKey;
-import org.bitcoinj.params.MainNetParams;
-
-import java.math.BigDecimal;
-import java.math.BigInteger;
-import java.text.DecimalFormat;
-import java.text.DecimalFormatSymbols;
-import java.text.NumberFormat;
-import java.text.ParseException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Locale;
-
 import piuk.blockchain.android.R;
 import piuk.blockchain.android.databinding.FragmentSendBinding;
 
@@ -749,10 +748,14 @@ public class SendFragment extends Fragment implements CustomKeypadCallback, Send
             binding.amountRow.amountBtc.setText(btcAmount);
             binding.amountRow.amountBtc.setSelection(binding.amountRow.amountBtc.getText().toString().length());
 
-            double btc_amount = 0.0;
+            double btc_amount;
             try {
-                btc_amount = monetaryUtil.getUndenominatedAmount(Double.parseDouble(binding.amountRow.amountBtc.getText().toString()));
+                NumberFormat numberFormat = NumberFormat.getNumberInstance(Locale.getDefault());
+                Number btcNumber = numberFormat.parse(binding.amountRow.amountBtc.getText().toString());
+                btc_amount = monetaryUtil.getUndenominatedAmount(btcNumber.doubleValue());
             } catch (NumberFormatException e) {
+                btc_amount = 0.0;
+            } catch (ParseException e) {
                 btc_amount = 0.0;
             }
 
